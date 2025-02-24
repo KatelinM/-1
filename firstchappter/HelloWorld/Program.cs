8aa6fb1342871579a35b32836f384ef7bfee9a63
21-code-online <86963107+21-code-online@users.noreply.github.com>
2025-02-24 10:14:05 +0100

    111
    1

---
+++ firstchappter/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+	 
+	static void Main1(int a)
+    {
+        Console.WriteLine("Hello, World!1");
+    }
+	static void Main1(string b)
+    {
+        Console.WriteLine("Hello, World!1");
+    }
+}
+
+
+
+
+
+
+
